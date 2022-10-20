62be52ba8bb0c95bd1edf9c680d1cb9bf94ceae8
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2022-10-20 20:32:16 +0700

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -33,10 +33,12 @@ handler.register = true
 
 export default handler */
 // ---------------------------new------------------------------------------------
+
 import fetch from 'node-fetch'
 import { youtubeSearch } from '@bochilteam/scraper'
 let handler = async (m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
-try {
+let fdoc = {quoted:{key : {participant : '0@s.whatsapp.net'},message: {documentMessage: {title: `${command}`}}}}
+//try {
   if (!text) throw `Use example ${usedPrefix}${command} gustixa`
   let vid = (await youtubeSearch(text)).video[0]
   if (!vid) throw 'Video/Audio Tidak ditemukan'
@@ -44,9 +46,10 @@ try {
   const url = 'https://www.youtube.com/watch?v=' + videoId
   let whmodsdev = `*${htki} PLAY ${htka}*
 
- 📌 *Title:* ${title}
+  📌 *Title:* ${title}
 🔗 *Url:* ${url}
-🖹 *Description:* ${description}
+📔 *Description:* ${description}
+
 ⏲️ *Published:* ${publishedTime}
 ⌚ *Duration:* ${durationH}
 👁️ *Views:* ${viewH}
@@ -55,28 +58,39 @@ try {
     ['🎶 Audio', `${usedPrefix}yta ${url} yes`],
     ['🎥 Video', `${usedPrefix}ytv ${url} yes`],
     ['🔎 Youtube Search', `${usedPrefix}yts ${text}`]
-], m, fakes)
-} catch {
+], m, fdoc)
+//} 
+/* catch {
 if (!text) throw 'Input Query'
   let vid = (await youtubeSearch(text)).video[0]
   if (!vid) throw 'Video/Audio Tidak Ditemukan'
   let { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
   let url = 'https://www.youtube.com/watch?v=' + videoId
   let ytLink = `https://yt-downloader.akkun3704.repl.co/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
-  let capt = `*Title:* ${title}\n*Published:* ${publishedTime}\n*Duration:* ${durationH}\n*Views:* ${viewH}\n*Url:* ${url}`
-  let buttons = [{ buttonText: { displayText: 'Video' }, buttonId: `${usedPrefix}ytv ${url}` }]
+  let capt = `*${htki} PLAY ${htka}*
+
+  📌 *Title:* ${title}
+🔗 *Url:* ${url}
+📔 *Description:* ${description}
+
+⏲️ *Published:* ${publishedTime}
+⌚ *Duration:* ${durationH}
+👁️ *Views:* ${viewH}
+  `
+  let buttons = [{ buttonText: { displayText: '🎶 Audio/Vn' }, buttonId: `${usedPrefix}yta ${url}` }, { buttonText: { displayText: '🎥 Video' }, buttonId: `${usedPrefix}ytv ${url}` }, { buttonText: { displayText: '🔎 Youtube Search' }, buttonId: `${usedPrefix}yts ${text}` }]
   let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: capt, footer: '_Audio on progress..._', buttons }, { quoted: m })
   // if (durationS > 4000) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(ytLink)}\n\n_Duration too long..._` }, { quoted: msg })
   conn.sendMessage(m.chat, { audio: { url: ytLink }, mimetype: 'audio/mpeg' }, { quoted: msg })
-}
+} */
 
 }
-handler.help = ['play', 'play'].map(v => v + ' <pencarian>')
-handler.tags = ['downloader']
-handler.command = /^(y((outube|((utu|t)b|t))play|tp)|play(yt)?)$/i
+handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
+handler.tags = ['downloader', 'limitmenu']
+handler.command = /^play2?$/i
 
 handler.exp = 0
 handler.limit = false
+handler.register = false
 
 export default handler
 
@@ -86,3 +100,4 @@ async function shortUrl(url) {
   if (!res.ok) throw false
   return await res.text()
 }
+
