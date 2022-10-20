2123424d447840769127ce6cfdb482ad16ac0386
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2022-10-20 20:33:51 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -52,7 +52,7 @@ Ketik ulang *${usedPrefix + command}* teks anda untuk mengubah teks lagi
 🚀 : Makanya bantu donasi buat support + beli apikey unlimited bjir :v
 
 Donasi :
-Nomor Developer : wa.me/6282127487538
+Nomor Developer : wa.me/6281368176386
 Nomor Owner Bot : wa.me/${nomorown}`
 
 // yang hapus nomor developer mudah-mudahan banyak crash
